3f86402b660511a99795bbbeb9b9ee5c2d6d4ce0
jadeyao0630 <jadeyao0630@gmail.com>
2023-12-12 19:19:15 +0800

    first commit

--- client/js/tools.js
+++
@@ -1,5 +0,0 @@
-const formatString = (template, ...args) => {
-    return template.replace(/{([0-9]+)}/g, function (match, index) {
-        return typeof args[index] === 'undefined' ? match : args[index];
-    });
-}
